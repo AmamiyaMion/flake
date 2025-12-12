8565c3263e33670938b4ffaf0fdb19258912b7d8
Amamiya Mion <amamiya_mion@outlook.com>
2025-12-12 23:22:37 +0800

    homeModules/ssh.nix: setup AOSC Buildbots SSH config via home-manager
    Signed-off-by: Amamiya Mion <amamiya_mion@outlook.com>

--- homeModules/ssh.nix
+++ homeModules/ssh.nix
@@ -6,6 +6,16 @@
 }:
 
 {
+  # Set up AOSC Buildbots ssh config
+  home.file.".ssh/config.d/aosc".source = pkgs.fetchurl {
+    url = "https://cdn.jsdelivr.net/gh/AOSC-Dev/Buildbots/ssh_config";
+    sha256 = "sha256-nZtB6KZMmJZKOJ2iXA/kSFsorgj+DcmxOhyzvrD0/+8=";
+  };
+  home.file.".ssh/known_hosts.d/aosc".source = pkgs.fetchurl {
+    url = "https://cdn.jsdelivr.net/gh/AOSC-Dev/Buildbots/ssh_known_hosts";
+    sha256 = "sha256-CyvCX3ucpfckN9730BjXR46AVPafmEHO3wC+T3xQjWM=";
+  };
+
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
@@ -15,5 +25,8 @@
     #       IdentityAgent ~/.1password/agent.sock
     # '';
     matchBlocks."*".identityAgent = "~/.1password/agent.sock";
+    matchBlocks."*".extraOptions = {
+      Include = "config.d/*";
+    };
   };
 }
