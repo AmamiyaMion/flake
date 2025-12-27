ac29da596cea3287563a665a16294d920b00fd73
Amamiya Mion <amamiya_mion@outlook.com>
2025-12-27 16:49:20 +0800

    setup `angrr`
    Signed-off-by: Amamiya Mion <amamiya_mion@outlook.com>

--- nixosModules/system/nix.nix
+++ nixosModules/system/nix.nix
@@ -36,10 +36,46 @@
   # Allow non-free software
   nixpkgs.config.allowUnfree = true;
 
+  services.angrr = {
+    enable = true;
+    settings = {
+      temporary-root-policies = {
+        direnv = {
+          path-regex = "/\\.direnv/";
+          period = "7d";
+        };
+        result = {
+          path-regex = "/result[^/]*$";
+          period = "3d";
+        };
+      };
+      profile-policies = {
+        system = {
+          profile-paths = [ "/nix/var/nix/profiles/system" ];
+          keep-since = "14d";
+          keep-latest-n = 5;
+          keep-booted-system = true;
+          keep-current-system = true;
+        };
+        user = {
+          enable = true; # Policies can be individually disabled
+          profile-paths = [
+            # `~` at the beginning will be expanded to the home directory of each discovered user
+            "~/.local/state/nix/profiles/profile"
+            "/nix/var/nix/profiles/per-user/root/profile"
+          ];
+          keep-since = "1d";
+          keep-latest-n = 1;
+        };
+        # You can define your own policies
+        # ...
+      };
+    };
+  };
   # Enable automatic gc
   nix.gc = {
     automatic = true;
-    options = "--delete-older-than 7d";
+    # options = "--delete-older-than 7d";
     dates = "daily";
   };
 
