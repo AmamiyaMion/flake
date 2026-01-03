12f94b44fa499c5eb187440c62eb4144a8efa581
Amamiya Mion <amamiya_mion@outlook.com>
2026-01-03 17:51:24 +0800

    fixup: nixosConfigurations/elena home manager not enabled quq
    Signed-off-by: Amamiya Mion <amamiya_mion@outlook.com>

--- nixosConfigurations/elena.nix
+++ nixosConfigurations/elena.nix
@@ -22,6 +22,7 @@
   networking.hostId = "ffafeebc"; # For zfs; Make it random!
 
   mion.systemPackages.office.enable = true;
+  mion.homeManager.enable = true;
   mion.homeManager.modules = with homeModules; [
     vscode
     sway
