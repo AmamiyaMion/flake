d9f1c86dc36462b5501c12f0cfcca0099580c4d9
雨宮 澪音 <amamiya_mion@aosc.io>
2026-01-16 21:32:40 +0800

    homeModules.sway: xkb: swap ctrl and capslock instead of win
    Signed-off-by: 雨宮 澪音 <amamiya_mion@aosc.io>

--- homeModules/sway.nix
+++ homeModules/sway.nix
@@ -56,7 +56,7 @@ in
           natural_scroll = "enabled";
         };
         "type:keyboard" = {
-          xkb_options = ''"ctrl:swap_lwin_lctl"'';
+          xkb_options = ''"ctrl:swapcaps"'';
         };
       };
       output = {
