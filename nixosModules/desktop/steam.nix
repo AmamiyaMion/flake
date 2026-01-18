4aa8e20bbb13540616043d6a1df7aa994582c9ce
雨宮 澪音 <amamiya_mion@aosc.io>
2026-01-18 15:44:16 +0800

    nixosModules.desktop.steam: fix fontconfig
    Signed-off-by: 雨宮 澪音 <amamiya_mion@aosc.io>

--- nixosModules/desktop/steam.nix
+++ nixosModules/desktop/steam.nix
@@ -7,6 +7,9 @@
 {
   programs.steam = {
     enable = true;
+    fontPackages = with pkgs; [
+      noto-fonts-cjk-sans
+    ];
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
