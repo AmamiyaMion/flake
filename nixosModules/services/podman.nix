b73552e5994aa43724267fd946c3ea0653988a2c
雨宮 澪音 <amamiya_mion@aosc.io>
2026-02-13 11:16:05 +0800

    nixosModules.services.podman: init
    Signed-off-by: 雨宮 澪音 <amamiya_mion@aosc.io>

---
+++ nixosModules/services/podman.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  virtualisation.containers.enable = true;
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
+  };
+}
--- nixosModules/users/mion.nix
+++ nixosModules/users/mion.nix
@@ -28,7 +28,8 @@ in
         "kvm"
         "adbusers"
         "dialout"
-      ];
+      ]
+      ++ lib.optionals config.virtualisation.podman.enable [ "podman" ];
       shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOrG7IrNH5u3LKXA/W9W/2yV+/lGXT9Ejl8FOhB28lus"
