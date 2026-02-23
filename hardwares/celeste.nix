e8c2743af02af6e9c3744920e640dc3b2583f2d4
雨宮 澪音 <amamiya_mion@aosc.io>
2026-02-23 23:24:14 +0800

    hardwares/celeste: nvidia: fix build for kernel 6.19
    Signed-off-by: 雨宮 澪音 <amamiya_mion@aosc.io>

--- hardwares/celeste.nix
+++ hardwares/celeste.nix
@@ -74,5 +74,25 @@
   '';
   environment.systemPackages = lib.mkOrder 699 [ pkgs.amdgpu_top ];
   hardware.graphics.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+  # for Linux Kernel 6.19
+  # https://github.com/cubewhy/nixos-dotfiles/blob/master/hardware%2Fnvidia.nix
+  hardware.nvidia.open = true;
+  hardware.nvidia.package =
+    let
+      base = config.boot.kernelPackages.nvidiaPackages.latest;
+      cachyos-nvidia-patch = pkgs.fetchpatch {
+        url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
+        sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
+      };
+
+      # Patch the appropriate driver based on config.hardware.nvidia.open
+      driverAttr = if config.hardware.nvidia.open then "open" else "bin";
+    in
+    base
+    // {
+      ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
+        patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
+      });
+    };
 }
