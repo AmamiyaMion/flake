1ceda29c8f5c7cc9fb1800f2ca9237b7f2446264
雨宮 澪音 <mion@amamiya.uk>
2026-07-17 23:01:48 +0800

    homeModules.ssh: update AOSC Dev ssh files hash
    Signed-off-by: 雨宮 澪音 <mion@amamiya.uk>

--- homeModules/ssh.nix
+++ homeModules/ssh.nix
@@ -8,11 +8,11 @@
   # Set up AOSC Buildbots ssh config
   home.file.".ssh/config.d/aosc".source = pkgs.fetchurl {
     url = "https://cdn.jsdelivr.net/gh/AOSC-Dev/Buildbots/ssh_config";
-    hash = "sha256-6pohGMYCDs4IkI3F+0lz7nEizbyxe/geWxv/9SLccJc=";
+    hash = "sha256-8LhvtGVfQy+BecLqdGfZ4ncjofcR4TxtILqoiUYEhgE=";
   };
   home.file.".ssh/known_hosts.d/aosc".source = pkgs.fetchurl {
     url = "https://cdn.jsdelivr.net/gh/AOSC-Dev/Buildbots/ssh_known_hosts";
-    hash = "sha256-ryCZdGTss5ly7of7upSRDaOcgC+q+N8GeRPIu+9g8C0=";
+    hash = "sha256-z9H+8U4MCfMpPtOW4MdcA+KamoMn3LkqpQVC/MaKneo=";
   };
 
   programs.ssh = {
